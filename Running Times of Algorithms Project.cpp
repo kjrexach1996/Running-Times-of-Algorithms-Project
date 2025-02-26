3988de218ebade8f5a1e76d37fbec64bf5008c74
Kevin Rexach <kaiorex@hotmail.com>
2025-02-26 15:16:02 -0500

    Updated Part 3.

--- Running Times of Algorithms Project.cpp
+++ Running Times of Algorithms Project.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <algorithm>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -16,13 +17,10 @@ int main()
 {
 	//Variable declarations representing size of vector N and 
 	//average times for each type of search
-	int N = 100;
+	int N = 1000000;
 	int sumRBS = 0;
 	int sumIBS = 0;
 	int sumSeqS = 0;
-
-	//Initializing a vector
-	vector<int> myVector;
 
 	//Creating a random number generator (rng)
 	mt19937 rng(random_device{}());
@@ -33,16 +31,83 @@ int main()
 	//Loop runs 10 times
 	for (int i = 0; i < 10; ++i)
 	{
-		//Fills the vector with N random integers ranging from 1 to N
+		//Initializing a vector
+		vector<int> myVector;
+
+		//Vector is filled with N random integers
 		for (int j = 0; j < N; ++j)
 		{
 			myVector.push_back(distributor(rng));
 		}
+		//Vector is sorted in ascending order
 		sort(myVector.begin(), myVector.end());
-		
-	}
 
-	
+		//***RBS***//
+		//Generating a random target value to find in vector
+		int target = distributor(rng);
+
+		cout << "Target for RBS: " << target << endl;
+
+		//Start time
+		auto start = chrono::high_resolution_clock::now();
+
+		//Beginning the search for the target using recursive binary search
+		int result = recursiveSearch(myVector, target, 0, myVector.size() - 1);
+
+		//End time
+		auto end = chrono::high_resolution_clock::now();
+
+		//Calculating the time of RBS and storing in sumRBS
+		double duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		sumRBS += duration;
+
+		cout << "sumRBS: " << sumRBS << endl << endl;
+
+		//***IBS***//
+		//Resetting random target value to find
+		target = distributor(rng);
+
+		cout << "Target for IBS: " << target << endl;
+
+		//Start time
+		start = chrono::high_resolution_clock::now();
+
+		//Beginning the search for the target using iterative binary search
+		result = iterativeSearch(myVector, target);
+
+		//End time
+		end = chrono::high_resolution_clock::now();
+
+		//Calculating the time of IBS and storing in sumIBS
+		duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		sumIBS += duration;
+
+		cout << "sumIBS: " << sumIBS << endl << endl;
+
+		//***SeqS***//
+		//Resetting random target to a 3rd value to find
+		target = distributor(rng);
+
+		cout << "Target for SeqS: " << target << endl;
+
+		//Start time
+		start = chrono::high_resolution_clock::now();
+
+		//Beginning the search for the target using iterative binary search
+		result = sequentialSearch(myVector, target);
+
+		//End time
+		end = chrono::high_resolution_clock::now();
+
+		//Calculating the time of IBS and storing in sumIBS
+		duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+		sumSeqS += duration;
+
+		cout << "sumSeqS: " << sumSeqS << endl << endl;
+	}
+	cout << "Average running time for recursive binary search: " << sumRBS / 10 << endl;
+	cout << "Average running time for iterative binary search: " << sumIBS / 10 << endl;
+	cout << "Average running time for sequential search: " << sumSeqS / 10 << endl;
 }
 
 int recursiveSearch(const vector<int>& vec, int value, int low, int high)
