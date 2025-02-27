dd6288173dd491616dfe0178607c8655288c07cd
Kevin Rexach <kaiorex@hotmail.com>
2025-02-27 16:44:56 -0500

    Part 3 of Running Time Algorithms Project, Rate of Growth, with updated comments. Number of integers in vector will be set to 5000 by default.

--- Running Times of Algorithms Project.cpp
+++ Running Times of Algorithms Project.cpp
@@ -1,23 +1,38 @@
-// Running Times of Algorithms Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Running Times of Algorithms Project.cpp (Part 3: Rate of Growth): This program takes the
+// three search algorithms from Parts 1 and 2 and uses them find a random target value
+// in a vector filled with a larger and larger set of random integers. The running times 
+// of each algorithm are recorded to compare each algorithms' rate of growth.
 
 #include <iostream>
 #include <random>
-#include <algorithm>
+#include <algorithm> //Included to sort vector
 #include <vector>
 #include <chrono>
 
 using namespace std;
 
+// Preconditions: Vector must be accessible and nonempty; target, low, and high must be integers;
+// at first, low must be 0 and high must be the last index in the vector; low and high must update
+// with the correct parameters as the vector halves during every recursive call.
+// Postconditions: original function call will return a -1 if target value was not found or
+// a value representing the index where the target was found; vector must stay the same.
 int recursiveSearch(const vector<int>& vec, int value, int low, int high);
+
+// Preconditions: Vector must be accessible and nonempty; target must be an integer;
+// Postconditions: function will return a -1 if target value was not found or
+// a value representing the index where the target was found; vector must stay the same.
 int iterativeSearch(const vector<int>& vec, int value);
+
+// Preconditions: Vector must be accessible and nonempty; target must be an integer;
+// Postconditions: function will have gone through every index in vector until target value
+// was found. If not found, returns -1.
 int sequentialSearch(const vector<int>& vec, int value);
 
 int main()
 {
 	//Variable declarations representing size of vector N and 
 	//average times for each type of search
-	int N = 5000;
+	int n = 5000;
 	int sumRBS = 0;
 	int sumIBS = 0;
 	int sumSeqS = 0;
@@ -26,27 +41,28 @@ int main()
 	mt19937 rng(random_device{}());
 
 	//Setting the range of random integers for distribution
-	uniform_int_distribution<int> distributor(1, N);
+	uniform_int_distribution<int> distributor(1, n);
 
-	//Loop runs 10 times
+	//Loop runs 10 times and takes average running times for each search algorithm
 	for (int i = 0; i < 10; ++i)
 	{
 		//Initializing a vector
 		vector<int> myVector;
 
-		//Vector is filled with N random integers
-		for (int j = 0; j < N; ++j)
+		//Vector is filled with n random integers
+		for (int j = 0; j < n; ++j)
 		{
 			myVector.push_back(distributor(rng));
 		}
+
 		//Vector is sorted in ascending order
 		sort(myVector.begin(), myVector.end());
+
+		cout << "Vector " << i + 1 << endl;
 
 		//***RBS***//
 		//Generating a random target value to find in vector
 		int target = distributor(rng);
-
-		cout << "Target for RBS: " << target << endl;
 
 		//Start time
 		auto start = chrono::high_resolution_clock::now();
@@ -61,13 +77,11 @@ int main()
 		double duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 		sumRBS += duration;
 
-		cout << "sumRBS: " << sumRBS << endl << endl;
+		cout << "sumRBS: " << sumRBS << " nanoseconds" << endl;
 
 		//***IBS***//
 		//Resetting random target value to find
 		target = distributor(rng);
-
-		cout << "Target for IBS: " << target << endl;
 
 		//Start time
 		start = chrono::high_resolution_clock::now();
@@ -82,13 +96,11 @@ int main()
 		duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 		sumIBS += duration;
 
-		cout << "sumIBS: " << sumIBS << endl << endl;
+		cout << "sumIBS: " << sumIBS << " nanoseconds" << endl;
 
 		//***SeqS***//
 		//Resetting random target to a 3rd value to find
 		target = distributor(rng);
-
-		cout << "Target for SeqS: " << target << endl;
 
 		//Start time
 		start = chrono::high_resolution_clock::now();
@@ -103,8 +115,11 @@ int main()
 		duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 		sumSeqS += duration;
 
-		cout << "sumSeqS: " << sumSeqS << endl << endl;
+		cout << "sumSeqS: " << sumSeqS << " nanoseconds" << endl;
+
+		cout << endl;
 	}
+	//Printing out average times for search algorithms
 	cout << "Average running time for recursive binary search: " << sumRBS / 10 << " nanoseconds" << endl;
 	cout << "Average running time for iterative binary search: " << sumIBS / 10 << " nanoseconds" << endl;
 	cout << "Average running time for sequential search: " << sumSeqS / 10 << " nanoseconds" << endl;
@@ -112,16 +127,18 @@ int main()
 
 int recursiveSearch(const vector<int>& vec, int value, int low, int high)
 {
+	//If false, target was not found; returns -1
 	if (low <= high)
 	{
+		//Halves the vector in two
 		int mid = low + ((high - low) / 2);
 
 		if (vec[mid] == value)
-			return mid;
-		else if (vec[mid] < value)
-			return recursiveSearch(vec, value, mid + 1, high);
+			return mid; //Returns the index where target was found
+		else if (vec[mid] < value) 
+			return recursiveSearch(vec, value, mid + 1, high); //Function call searches back half of vector
 		else
-			return recursiveSearch(vec, value, low, mid - 1);
+			return recursiveSearch(vec, value, low, mid - 1); //Function call searches front half of vector
 	}
 	else
 		return -1;
@@ -129,25 +146,28 @@ int recursiveSearch(const vector<int>& vec, int value, int low, int high)
 
 int iterativeSearch(const vector<int>& vec, int value)
 {
+	//Initiates beginning and end indexes of vector
 	int low = 0;
 	int high = vec.size() - 1;
 
 	while (low <= high)
 	{
+		//Halves the vector in two
 		int mid = low + ((high - low) / 2);
 
 		if (vec[mid] == value)
-			return mid;
+			return mid; //Returns the index where target was found
 		else if (vec[mid] < value)
-			low = mid + 1;
+			low = mid + 1; //New beginning index
 		else
-			high = mid - 1;
+			high = mid - 1; //New end index
 	}
 	return -1;
 }
 
 int sequentialSearch(const vector<int>& vec, int value)
 {
+	//Searches through every index of vector until target is found/not found
 	for (int i = 0; i < vec.size(); ++i)
 	{
 		if (vec[i] == value)
